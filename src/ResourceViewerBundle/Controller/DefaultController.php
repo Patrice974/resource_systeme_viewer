53f17e47565f76c14668b4ebce684ceef71d84af
GRONDIN Patrice <pgrondin@globalliance-solutions.com>
2017-03-30 15:06:30 +0400

    create action for async. request

--- src/ResourceViewerBundle/Controller/DefaultController.php
+++ src/ResourceViewerBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace ResourceViewerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -13,5 +14,13 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('ResourceViewerBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/resource_system")
+     */
+    public function resourceSystemAction()
+    {
+        return new JsonResponse(array('name' => "toto"));
     }
 }
