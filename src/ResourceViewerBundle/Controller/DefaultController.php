24e33c1b14cf91f5477fa9940e09e59c4ba2a39d
GRONDIN Patrice <pgrondin@globalliance-solutions.com>
2017-03-30 15:46:38 +0400

    try to get cpu/memony info

--- src/ResourceViewerBundle/Controller/DefaultController.php
+++ src/ResourceViewerBundle/Controller/DefaultController.php
@@ -21,6 +21,16 @@ class DefaultController extends Controller
      */
     public function resourceSystemAction()
     {
-        return new JsonResponse(array('name' => "toto"));
+        $cpu_usage = sys_getloadavg()[0];
+
+        $free = shell_exec('free');
+        $free = (string)trim($free);
+        $free_arr = explode("\n", $free);
+        $mem = explode(" ", $free_arr[1]);
+        $mem = array_filter($mem);
+        $mem = array_merge($mem);
+        $memory_usage = $mem[2]/$mem[1]*100;
+
+        return new JsonResponse(array('name' => "toto", 'cpu_usage' => $cpu_usage, 'memory_usage' => $memory_usage));
     }
 }
