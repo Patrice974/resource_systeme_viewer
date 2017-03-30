5b0fe7371d3e70cec2a764f0c349211c02e8f0cd
GRONDIN Patrice <pgrondin@globalliance-solutions.com>
2017-03-30 16:46:10 +0400

    add linfo

--- src/ResourceViewerBundle/Controller/DefaultController.php
+++ src/ResourceViewerBundle/Controller/DefaultController.php
@@ -21,16 +21,13 @@ class DefaultController extends Controller
      */
     public function resourceSystemAction()
     {
-        $cpu_usage = sys_getloadavg()[0];
+        $linfo = new \Linfo\Linfo;
+        $parser = $linfo->getParser();
 
-        $free = shell_exec('free');
-        $free = (string)trim($free);
-        $free_arr = explode("\n", $free);
-        $mem = explode(" ", $free_arr[1]);
-        $mem = array_filter($mem);
-        $mem = array_merge($mem);
-        $memory_usage = $mem[2]/$mem[1]*100;
-
-        return new JsonResponse(array('name' => "toto", 'cpu_usage' => $cpu_usage, 'memory_usage' => $memory_usage));
+        return new JsonResponse(array(
+          'cpu' => $parser->getCPU(),
+          'disk' => $parser->getHD(),
+          'ram' => $parser->getRam()
+        ));
     }
 }
